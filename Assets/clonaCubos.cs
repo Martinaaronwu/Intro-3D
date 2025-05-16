5959c2d9e639ecb627f1371c16e986a917c246ca
Martinaaronwu <166827155+Martinaaronwu@users.noreply.github.com>
2025-05-16 10:35:37 -0300

    Clonar cubos en un plane!

---
+++ Assets/clonaCubos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonaCubos : MonoBehaviour
+{
+    public GameObject cuboprefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    public void clonarCubos()
+    {
+        Instantiate(cuboprefab);
+    }
+}
